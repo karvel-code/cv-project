6a23cba3d733d0438e1cb570eb1259f1fdb80ea9
karvel-code <velgithinji@gmail.com>
2021-10-19 09:53:34 +0300

    Add condition for form rendering

--- src/Components/general.js
+++ src/Components/general.js
@@ -6,10 +6,13 @@ import ReactDOM from "react-dom";
         super(props);
         this.state = {name: ' ',
                       email:' ',
-                      number:' '  
+                      number:' ',
+                      formToggle:true,
+                      contentToggle:false  
                     };
         this.handleNameChange = this.handleNameChange.bind(this) 
-        this.handleEmailChange = this.handleEmailChange.bind(this)           
+        this.handleEmailChange = this.handleEmailChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)        
     }; 
 
     handleNameChange(event) {
@@ -23,65 +26,74 @@ import ReactDOM from "react-dom";
             email: event.target.value
         });
     };
+    
+    handleSubmit(event) {
+       console.log(this.state)
+        event.preventDefault();
+    }
 
     render () {
-        return (
-            <div>
-                <h1>CV Applicattion</h1>
-                
-                <form>
-                <h2>General Info</h2>
-                    <label>
-                        Name:
-                        <input type="text" name="name" onChange={this.handleNameChange}></input>
-                    </label>
-                    <label>
-                        Email:
-                        <input type="email" name="email" onChange={this.handleEmailChange}></input>
-                    </label>
-                    <label>
-                        Phone Number:
-                        <input type="number" name="number"></input>
-                    </label>
-                    <input type="submit" value="Submit" />
-                <h2>Educational Experience</h2>
-                    <label>
-                        School Name:
-                        <input type="text" name="school-name"></input>
-                    </label>
-                    <label>
-                        Title of Study:
-                        <input type="text" name="title"></input>
-                    </label>
-                    <label>
-                        Date of Study:
-                        <input type="text" name="date-of-study"></input>
-                    </label>
-                    <input type="submit" value="Submit" />
-                    <h2>Practical Experience</h2>
-                    <label>
-                        Company Name:
-                        <input type="text" name="company"></input>
-                    </label>
-                    <label>
-                        Position Title:
-                        <input type="text" name="position-title"></input>
-                    </label>
-                    <label>
-                        Date Started:
-                        <input type="number" name="date-started"></input>
-                    </label>
-                    <label>
-                        Date Ended:
-                        <input type="number" name="date-started"></input>
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
-
-                <h1>{this.state.name}</h1>
-                <h1>{this.state.email}</h1>
-            </div>
-        )
+        if (this.state.formToggle == true) {
+            return (
+                <div>
+                    <h1>CV Applicattion</h1>
+                    
+                    <form onSubmit={this.handleSubmit}>
+                    <h2>General Info</h2>
+                        <label>
+                            Name:
+                            <input type="text" name="name" onChange={this.handleNameChange} />
+                        </label>
+                        <label>
+                            Email:
+                            <input type="email" name="email" onChange={this.handleEmailChange}/>
+                        </label>
+                        <label>
+                            Phone Number:
+                            <input type="number" name="number" />
+                        </label>
+                        <input type="submit" value="Submit" />
+                    <h2>Educational Experience</h2>
+                        <label>
+                            School Name:
+                            <input type="text" name="school-name"></input>
+                        </label>
+                        <label>
+                            Title of Study:
+                            <input type="text" name="title"></input>
+                        </label>
+                        <label>
+                            Date of Study:
+                            <input type="text" name="date-of-study"></input>
+                        </label>
+                        <input type="submit" value="Submit" />
+                        <h2>Practical Experience</h2>
+                        <label>
+                            Company Name:
+                            <input type="text" name="company"></input>
+                        </label>
+                        <label>
+                            Position Title:
+                            <input type="text" name="position-title"></input>
+                        </label>
+                        <label>
+                            Date Started:
+                            <input type="number" name="date-started"></input>
+                        </label>
+                        <label>
+                            Date Ended:
+                            <input type="number" name="date-started"></input>
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
+    
+                    <h1>{this.state.name}</h1>
+                    <h1>{this.state.email}</h1>
+                    <h1>{this.state.number}</h1>
+                </div>
+            )
+        }
+  
     }
 }
 
